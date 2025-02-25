f989e47dbd4fcbf63ac3e47664927fa4979f6ff1
Rango Mango <mangorango144@gmail.com>
2025-02-25 23:55:59 +0200

    refactor: convert Navbar to TypeScript

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { UserStatus } from "./UserStatus";
 import { CartStatus } from "./CartStatus";
 
-export function Navbar() {
+export function Navbar(): JSX.Element {
   return (
     <nav className="flex justify-start items-center bg-white shadow-md mx-auto lg:mt-4 px-4 sm:px-14 lg:px-7 lg:rounded-full w-full lg:w-[95%] xl:w-8/12 max-w-[1268px] h-[56px] font-medium text-black text-xs sm:text-base">
       <Link to="/">Home</Link>
