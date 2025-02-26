a43b8d45d460f14488aa0739c7306fcffca4751d
Rango Mango <mangorango144@gmail.com>
2025-02-26 12:07:59 +0200

    refactor: convert Home to TypeScript

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-export function Home() {
+export function Home(): JSX.Element {
   return (
     <section className="space-x-6 pt-10 font-bold text-center animate-slideUpFadeIn">
-      <p className="bg-clip-text bg-neutral-400 bg-gradient-to-b from-neutral-950 mb-10 pb-3 text-3xl text-transparent sm:text-5xl 2xl:text-7xl">
+      <p className="bg-neutral-400 bg-clip-text bg-gradient-to-b from-neutral-950 mb-10 pb-3 text-transparent text-3xl sm:text-5xl 2xl:text-7xl">
         Project: Shopping Cart
         <br />
         from The Odin Project
@@ -21,7 +21,7 @@ export function Home() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="inline-block border-2 border-slate-400 bg-transparent hover:bg-slate-100 shadow-lg px-4 py-2 rounded-full text-neutral-600">
+        <button className="inline-block bg-transparent hover:bg-slate-100 shadow-lg px-4 py-2 border-2 border-slate-400 rounded-full text-neutral-600">
           Assignment reference
         </button>
       </a>
